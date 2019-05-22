fde19cbf12604e5f06da9ef04718a8412c8d1242
Jacob Brehm <jake.m.brehm@gmail.com>
2019-05-21 21:45:43 -0400

    Rearranged the repository.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-README = (HERE / 'README.md').read_text()
+README = (HERE / 'readme.md').read_text()
 
 setup(
      name='lemons',
